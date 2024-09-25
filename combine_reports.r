9459db6d31fc5b548462f232efaa1ea14dba4ff9
Julie Christie <julie@resolvephilly.org>
2024-09-25 11:54:40 -0400

    update analysis

--- combine_reports.r
+++ combine_reports.r
@@ -51,10 +51,6 @@ view(all_reports)
 # Understand how many rows did not have successful pulls of known status
 sum(is.na(all_reports))
 
-# Rearrange the table to be more readable
-all_reports <- all_reports %>%
-  select(status, county, incident_date, prior_status, narrative)
-
 # Identify the county totals
 county_totals <- all_reports %>%
   group_by(county) %>%
@@ -78,14 +74,63 @@ all_reports[1250, 2] <- "Monroe"
 
 all_reports[1195, 2] <- "Wayne"
 
+# Spot clean for date formats
+all_reports$incident_date <- gsub("January", "Jan.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("February", "Feb.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("March", "Mar.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("April", "Apr.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("June", "Jun.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("July", "Jul.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("August", "Aug.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("September", "Sep.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("October", "Oct.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("November", "Nov.", all_reports$incident_date)
+
+all_reports$incident_date <- gsub("December", "Dec.", all_reports$incident_date)
+
+# Fix leading 0s
+all_reports$incident_date <- gsub(" 0", " ", all_reports$incident_date)
+
+# Fix bad Feb. dates
+all_reports$incident_date <- gsub("Feb. 30", "Feb. 1", all_reports$incident_date)
+
+# Remove periods
+all_reports$incident_date <- gsub("\\.", "", all_reports$incident_date)
+
+# Format incident_date as datetime
+guess_formats(all_reports$incident_date, "mdy")
+all_reports$incident_date <- mdy(all_reports$incident_date)
+
+# Get year and quarter for all cases
+all_reports <- all_reports %>%
+  mutate(year = year(incident_date),
+         quarter = quarter(incident_date,
+            type = "date_first",
+            fiscal_start = 1))
+
+
+# Rearrange the table to be more readable
+all_reports <- all_reports %>%
+  select(status, county, incident_date, year, quarter, prior_status, narrative)
+
 # Filter for Philadelphia reports
 phl_reports <- all_reports %>%
   filter(county == "Philadelphia") %>%
-  mutate(year = substr(incident_date, nchar(incident_date) - 3, nchar(incident_date)))
+  mutate(year = year(incident_date))
+  # mutate(year = substr(incident_date, nchar(incident_date) - 3, nchar(incident_date)))
 
 sum(is.na(phl_reports))
 
-# Graph the timeline of PHL cases
+# Graph the timeline of annual PHL cases
 
 phl_timeline <- phl_reports %>%
   group_by(year) %>%
@@ -100,6 +145,8 @@ ggplot(data = phl_timeline,
     x = "Year",
     title = "Philadelphia fatality and near fatalities")
 
+# Graph annual PHL cases by fatality status
+
 phl_timeline2 <- phl_reports %>%
   group_by(year, status) %>%
   summarise(cases = n())
@@ -111,6 +158,20 @@ ggplot(data = phl_timeline2,
   labs(
     y = "Number of cases",
     x = "Year",
+    title = "Philadelphia fatality and near fatalities")
+
+# Graph quarterly PHL cases in total
+phl_timeline_q <- phl_reports %>%
+  group_by(quarter) %>%
+  summarise(cases = n())
+
+ggplot(data = phl_timeline,
+       aes(x = quarter, y = cases, group = 1)) +
+  geom_line(linewidth = 1.5, alpha = .75) +
+  #Labels
+  labs(
+    y = "Number of cases",
+    x = "Quarter",
     title = "Philadelphia fatality and near fatalities")
 
 # Write .csv files of data
